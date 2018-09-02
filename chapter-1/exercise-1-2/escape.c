6e28a55dc4086196612ac1c257ade49e2f719ef4
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-09-02 13:39:48 +0200

    Update escape.c to doesn't bother with \e escape sequence
    * cl doesn't recognize \e as a valid escape sequence, raises a warning.

--- chapter-1/exercise-1-2/escape.c
+++ chapter-1/exercise-1-2/escape.c
@@ -19,8 +19,8 @@ main()
 	printf("\\a: '\a'\n");
 	printf("\\b: '\b'\n");
 	/* printf("\\c: '\c'\n");
-	printf("\\d: '\d'\n"); */
-	printf("\\e: '\e'\n");
+	printf("\\d: '\d'\n");
+	printf("\\e: '\e'\n"); */
 	printf("\\f: '\f'\n");
 	/* printf("\\g: '\g'\n");
 	printf("\\h: '\h'\n");
