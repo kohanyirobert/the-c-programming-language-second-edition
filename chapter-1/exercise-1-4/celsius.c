829721f2f976d259404dd1d4eba30611d874c3c3
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-09-02 13:41:02 +0200

    Update celsius.c, made celsius var an int instead of float

--- chapter-1/exercise-1-4/celsius.c
+++ chapter-1/exercise-1-4/celsius.c
@@ -2,8 +2,8 @@
 
 main()
 {
-	float celsius, fahr;
-	int lower, upper, step;
+	float fahr;
+	int celsius, lower, upper, step;
 
 	lower = 0;
 	upper = 300;
@@ -13,8 +13,8 @@ main()
 
 	celsius = lower;
 	while (celsius <= upper) {
-		fahr = (9.0 / 5.0) * celsius + 32.0;
-		printf("%3.0f %6.1f\n", celsius, fahr);
+		fahr = (9.0 / 5.0) * celsius + 32;
+		printf("%3d %6.1f\n", celsius, fahr);
 		celsius += step;
 	}
 }
