f966ce7c600d6da420515600db872bfa46922668
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-09-02 13:42:56 +0200

    Update fahrenheit.c, made fahr var to an int instead of float

--- chapter-1/exercise-1-3/fahrenheit.c
+++ chapter-1/exercise-1-3/fahrenheit.c
@@ -2,8 +2,8 @@
 
 main()
 {
-	float fahr, celsius;
-	int lower, upper, step;
+	float celsius;
+	int fahr, lower, upper, step;
 
 	lower = 0;
 	upper = 300;
@@ -13,8 +13,8 @@ main()
 
 	fahr = lower;
 	while (fahr <= upper) {
-		celsius = (5.0 / 9.0) * (fahr - 32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
+		celsius = (5.0 / 9.0) * (fahr - 32);
+		printf("%3d %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
